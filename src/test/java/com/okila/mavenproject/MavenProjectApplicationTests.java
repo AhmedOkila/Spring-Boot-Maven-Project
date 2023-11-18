a1f9df86a82b269e3e89280ecec6788e71d20d84
Ahmed Elsaid <ahmedelsaid3123@gmail.com>
2023-11-18 14:30:44 +0200

    Adding the Project

---
+++ src/test/java/com/okila/mavenproject/MavenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okila.mavenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
